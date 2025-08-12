79b1841c900d7b8865857c286950adb8444f42e5
Mauricio-Nettoo <mauricio.almeida.dev@gmail.com>
2025-08-12 09:58:51 -0300

    adds euclidesMDC

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class Main {
     public static void main (String[] args) {
@@ -23,5 +25,16 @@ public class Main {
 
     public static BigInteger totient(BigInteger n1, BigInteger n2) {
         return n1.subtract(BigInteger.ONE).multiply(n2.subtract(BigInteger.ONE));
+    }
+
+    public static BigInteger euclidesMDC(BigInteger n1, BigInteger n2) {
+        while (n1.compareTo(n2) != 0) {
+            if (n1.compareTo(n2) > 0) {
+                n1 = n1.subtract(n2);
+            } else {
+                n2 = n2.subtract(n1);
+            }
+        }
+        return n1;
     }
 }
