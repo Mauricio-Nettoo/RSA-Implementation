4f2c037e6012e6b623119c02fcbe082a221c1e85
Mauricio-Nettoo <mauricio.almeida.dev@gmail.com>
2025-08-12 09:23:05 -0300

    adds findD method to find the "d" number used in the private key

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,14 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public static int findD(int e, int totient) {
+        int i = 1;
+        while (true) {
+            if (e * i % totient == 1) {
+                return i;
+            }
+            i++;
+        }
+    }
 }
